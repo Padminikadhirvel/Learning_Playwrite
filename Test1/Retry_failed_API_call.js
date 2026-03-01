2fa7d3f4f7cd2d15c7f78bd3824f667395b1aa81
Padminikadhirvel <adhithmini@gmail.com>
2026-03-01 10:58:38 +0530

    test2 q3

--- Test1/Retry_failed_API_call.js
+++ Test1/Retry_failed_API_call.js
@@ -1,8 +1,20 @@
-let attempt=1;
+let attempt=0;
 let success=false;
-const maxAttempts=3;
+const maxAttempts=5;
 do{
+    attempt++;
     const randomNum=Math.random();
     console.log(`Attempt ${attempt}: Random number generated: ${randomNum}`);
-    if(randomNum>0.){
+    if(randomNum>0.6){
+        success=true;
+        console.log("API call success.");
+    }else{
+        console.log("API call failed. Retrying...");
+    }
+}while(!success && attempt < maxAttempts);
+
+if(success){
+    console.log("API call succeeded after " + attempt + " attempts.");
+}else{
+    console.log("API call failed after " + maxAttempts + " attempts.");
 }
